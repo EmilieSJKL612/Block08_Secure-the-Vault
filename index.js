c30c50b1a4af235aaa3962f441eb8cf2b323e12c
SJKL612 <emily.chiang@colostate.edu>
2025-04-29 14:46:16 -0600

    finished index.js by following README.md  instructions

---
+++ index.js
@@ -0,0 +1,33 @@
+
+/* https://www.w3schools.com/js/js_const.asp => const variables must be assigned a value when they are declared */
+let code1; 
+code1 = 10;
+code1 = 7 + 3;
+
+let code2 = 40;
+code2 = 100 / 2 - 10;
+
+let code3 = 39;
+code3 = 9 % 10 + 30;
+
+// wonder if the right " was intentionally ommitted in the instructions... 
+const message = `"The vault has been secured. The combination is:.`
+
+//concatenate
+let codeA = 10-40-39;
+codeA = code1 - code2 - code3;
+
+//template literal (like f-strings...)
+let codeB = 10-40-39;
+codeB = `${code1} - ${code2} - ${code3}`;
+
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
+
+
+
+
